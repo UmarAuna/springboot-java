ff416a9736dfaa1acbc5ec797eb7dd7013ea4b3e
Aleks Rutins <aleks@rutins.com>
2023-07-07 16:40:55 -0400

    first commit

---
+++ src/test/java/app/railway/springboot/java/SpringbootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package app.railway.springboot.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
